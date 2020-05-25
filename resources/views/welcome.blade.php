010d89f76ddbc90b7b1b4fd943ed1d47dae58b4d
Luca Rossi <lucaros97@gmail.com>
2020-05-25 09:20:41 +0200

    Installed Laravel. Installed TALL

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
+        <div class="flex items-center justify-center">
+            <div class="flex flex-col justify-around">
+                <div class="space-y-6">
+                    <h1 class="text-5xl font-extrabold tracking-wider text-center text-gray-600">
+                        WIP
+                    </h1>
+                    <ul class="list-reset">
+                        <li class="inline px-4">
+                            <a href="https://lucarossi.dev" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">lucarossi.dev</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
